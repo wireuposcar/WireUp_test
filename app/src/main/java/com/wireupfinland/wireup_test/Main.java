b9da57e7e0eb47937de405a2f1d14041a96737a4
Benjamin Finell <benjamin.finell@gmail.com>
2018-06-11 23:05:02 +0300

    Added stuff to SetProject,Main and added appropiate id names on a few layouts

--- app/src/main/java/com/wireupfinland/wireup_test/Main.java
+++ app/src/main/java/com/wireupfinland/wireup_test/Main.java
@@ -8,23 +8,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Main extends AppCompatActivity {
+import java.util.Set;
 
+public class Main extends AppCompatActivity {
+    private FloatingActionButton newProject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newProject = (FloatingActionButton) findViewById(R.id.newProject);
+        startSetProject();
     }
     private void startSetProject() {
-        Intent project = new Intent(this, SetProject.class);
-        startActivity(project);
-    }
+
+
+        newProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(getApplicationContext(), SetProject.class);
+                startActivity(intent);
+
+            }
+        });
+        }
     private void DisplayProject(){
         //TODO fetch and display projects from database
     }
